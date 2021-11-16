8faffd99c050dabf920c8c2e1a2ebac1eaec3a0c
skander-turki <78934641+skander-turki@users.noreply.github.com>
2021-11-16 12:40:48 +0100

    Add files via upload

---
+++ Spring/src/main/java/tn/esprit/spring/entity/detailFacture.java
@@ -0,0 +1,125 @@
+package tn.esprit.spring.entity;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name= "detailFacture")
+public class detailFacture implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column(name="idDetailFacture")
+	private Long idDetailFacture;
+	
+	@Column(name="qte")
+	private Integer qte;
+	
+	@Column(name="prixTotal")
+	private Float prixTotal;
+	
+	@Column(name="pourcentageRemise")
+	private Integer pourcentageRemise;
+	
+	@Column(name="montantRemise")
+	private Float montantRemise;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="detailFactures")
+	private Set<Produit> produits;
+	
+	@ManyToOne
+	Facture factures;
+
+	public detailFacture() {
+		super();
+	}
+
+	public detailFacture( Integer qte, Float prixTotal, Integer pourcentageRemise,
+			Float montantRemise, Set<Produit> produits, Facture factures) {
+		super();
+		this.qte = qte;
+		this.prixTotal = prixTotal;
+		this.pourcentageRemise = pourcentageRemise;
+		this.montantRemise = montantRemise;
+		this.produits = produits;
+		this.factures = factures;
+	}
+
+	public Long getIdDetailFacture() {
+		return idDetailFacture;
+	}
+
+	public void setIdDetailFacture(Long idDetailFacture) {
+		this.idDetailFacture = idDetailFacture;
+	}
+
+	public Integer getQte() {
+		return qte;
+	}
+
+	public void setQte(Integer qte) {
+		this.qte = qte;
+	}
+
+	public Float getPrixTotal() {
+		return prixTotal;
+	}
+
+	public void setPrixTotal(Float prixTotal) {
+		this.prixTotal = prixTotal;
+	}
+
+	public Integer getPourcentageRemise() {
+		return pourcentageRemise;
+	}
+
+	public void setPourcentageRemise(Integer pourcentageRemise) {
+		this.pourcentageRemise = pourcentageRemise;
+	}
+
+	public Float getMontantRemise() {
+		return montantRemise;
+	}
+
+	public void setMontantRemise(Float montantRemise) {
+		this.montantRemise = montantRemise;
+	}
+
+	public Set<Produit> getProduits() {
+		return produits;
+	}
+
+	public void setProduits(Set<Produit> produits) {
+		this.produits = produits;
+	}
+
+	public Facture getFactures() {
+		return factures;
+	}
+
+	public void setFactures(Facture factures) {
+		this.factures = factures;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	
+	
+}
+
